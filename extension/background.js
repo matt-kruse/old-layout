d7305bba45e4e07f3657afa1c85ea07633226b17
Matt Kruse <matt@mattkruse.com>
2020-11-15 09:24:21 -0600

    Version 7.0

--- extension/background.js
+++ extension/background.js
@@ -15,11 +15,137 @@ api.runtime.onInstalled.addListener(function(details){
   else if ("update"===details.reason) {
     var show_update = true;
 
-    // Don't launch on update from 6.x to 6.x because it is a minor fix
-    if (/6\./.test(previousVersion)) { show_update = false; }
+    // Don't launch on update from 7.x to 7.x because it is a minor fix
+    //if (/7\./.test(previousVersion)) { show_update = false; }
 
     if (show_update) {
-      api.tabs.create({url: "https://OldLayout.com/update.html?version=6"});
+      api.tabs.create({url: "https://OldLayout.com/update.html?version=7"});
     }
   }
 });
+
+/*b-opera
+// This is a hack to fix the problem of opening Facebook via Speed Dial in Opera
+// In that case, extension background pages apparetly don't run, so we need to bounce to a new tab
+var opera_startpage_tab_ids = {};
+api.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+  if (details.url==="chrome://.*?/") {
+    opera_startpage_tab_ids[details.tabId] = true;
+  }
+  if (details.url==="https://www.facebook.com/" && opera_startpage_tab_ids[details.tabId]) {
+    delete opera_startpage_tab_ids[details.tabId];
+    api.tabs.create( { url: "https://www.facebook.com/home.php?old-layout-redirect" } );
+    api.tabs.remove( details.tabId );
+  }
+});
+b-opera*/
+
+/*
+// USER-AGENT FIX
+// ==============
+var useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko";
+var switch_agent = true;
+var add_referer = true;
+function rewriteUserAgentHeader(o) {
+  let add_referer = true;
+  if (o.url && /ol-rdr/.test(o.url)) {
+    return;
+  }
+  for (let header of o.requestHeaders) {
+    if (switch_agent && header.name.toLowerCase() === "user-agent") {
+      header.value = useragent;
+    }
+    if (add_referer && header.name.toLowerCase() === "referer") {
+      header.value = "https://www.facebook.com/";
+      add_referer = false;
+    }
+  }
+  if (add_referer) {
+    o.requestHeaders.push({name: "Referer", value: "https://www.facebook.com/"});
+  }
+  return {"requestHeaders": o.requestHeaders};
+}
+let sendHeadersOptions = ["blocking", "requestHeaders"];
+try {
+  if (api.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty("EXTRA_HEADERS")) {
+    sendHeadersOptions.push("extraHeaders");
+  }
+} catch (e) { }
+api.webRequest.onBeforeSendHeaders.addListener(
+  rewriteUserAgentHeader,
+  {urls: ["*://*.facebook.com/*"]},
+  sendHeadersOptions
+);
+*/
+
+/*
+// INTERNAL REDIRECT FIX
+// =====================
+let do_redirect = true;
+function clean(url) {
+  url = url.replace(/\/\//g,'/');
+  return url;
+}
+function redirect(o) {
+  if (!do_redirect) { return; }
+  let url = o.url;
+  if (/\/(messages|messaging)\//.test(url)) { return; }
+
+  let m = /https:\/\/m\./.test(url);
+  let has_params = /\?/.test(url);
+  let path = url.replace(/^http.*?facebook\.com/,'');
+  let parts = path.split('?');
+  path = parts[0] || '';
+  let params = parts[1] || '';
+  let redirect_url = "https://www.facebook.com/home.php";
+
+  // If we're trying to be on the newsfeed, we need to be on home.php
+  if (path==="/" && !/next=/.test(params)) {
+    return {
+      "redirectUrl": redirect_url
+    };
+  }
+  // If we got redirected to a mobile profile page
+  if (m && /__tn__=[^&]*C-R/.test(params)) {
+    return {
+      "redirectUrl": clean("https://www.facebook.com/" + path + "/timeline")
+    }
+  }
+  // If we get redirected to some other mobile page, try to avoid it
+  if (m && !/ol-rdr/.test(url)) {
+    return {
+      "redirectUrl": clean("http://www.facebook.com/" + path + "?" + params + (has_params ? "&" : "") + "ol-rdr")
+    }
+  }
+}
+let requestOptions = ["blocking"];
+api.webRequest.onBeforeRequest.addListener(
+  redirect,
+  {urls: ["*://*.facebook.com/*"]},
+  requestOptions
+);
+*/
+
+function reloadFacebookTabs() {
+  api.tabs.query({"url": "https://*.facebook.com/*"}, function(tabs) {
+    if (!tabs || !tabs.length) {
+      return;
+    }
+    tabs.forEach((t) => {
+      try {
+        api.tabs.reload(t.id);
+      } catch (e) {
+      }
+    });
+  });
+}
+
+function getStatus() {
+  return switch_agent;
+}
+function enable() {
+  switch_agent = true;
+}
+function disable() {
+  switch_agent = false;
+}
--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,26 @@
 let enabled = true;
-let version = 6.4;
+let version = 7.0;
+let date = "2020-11-15";
+let $ = sel=>document.querySelector(sel);
+let faq = '<a href="https://oldlayout.com/#faq" target="_blank" style="font-weight:bold;text-decoration:underline;">Old Layout FAQ</a>';
+let review_note = `<b>Please do not leave a negative review!</b> This fix works for many people, but not all. I'm sorry it didn't work for you, but it will work for others. Negative reviews hurt the chances of other people finding a fix. Thanks.`;
+let simulate_missing_data = false;
+let simulate_failure = true;
+let simulate_false_positive = false;
+
 addEventListener('load', function() {
-  chrome.storage.local.get('disabled', function(o) {
+  if (
+    !/www\.facebook\.com\/?$/.test(location.href) &&
+    !/www\.facebook\.com\/\?/.test(location.href) &&
+    !/www\.facebook\.com\/home/.test(location.href)
+    ) {
+    return;
+  }
+  // Don't run on login page
+  if ($('.UIPage_LoggedOut') || $('form[action^="/login"]')) {
+    return;
+  }
+  chrome.storage.local.get(['disabled'], function(o) {
     if (o.disabled && version <= o.disabled) {
       enabled = false;
     }
@@ -15,39 +34,52 @@ function add_panel() {
   if (document.querySelector('#pagelet_bluebar')) {
     return;
   }
-  let div = document.createElement('div');
-  div.id="oldlayout";
-
-  let header = document.createElement('div');
-  header.id = "oldlayout-header";
-  header.textContent = "Old Layout "+version;
-  div.appendChild(header);
-
-  let button = document.createElement('button');
-  button.id="oldlayout-button";
-  button.textContent = "Switch to Old Layout";
-  button.addEventListener('click',old_layout_switch);
-  div.appendChild(button);
-
-  let msg = document.createElement('div');
-  msg.id = "oldlayout-msg";
-  msg.textContent = "Click the button above to trigger Facebook's internal mechanism that allows users to switch back to the old layout for 48 hours. When this period ends, this message will appear again and you can trigger it again to switch.";
-  div.appendChild(msg);
-
-  let disable = document.createElement('button');
-  disable.id="oldlayout-disable-button";
-  disable.textContent = "Disable This Popup";
-  disable.addEventListener('click',disable_popup);
-  div.appendChild(disable);
-
+  let message = `<b>IMPORTANT! PLEASE READ!</b><br>
+    <p>Click the "Switch to Old Layout" below to trigger Facebook's internal mechanism that allows users to switch back to the old layout for 48 hours. When this period ends, this message will appear again and you can trigger it again to switch.</p>
+    <p><b>NOTE: This does NOT work for all Facebook accounts!</b> Unfortunately, Facebook will not allow some accounts to switch to the old layout. This is beyond our control, and not an issue with the Old Layout extension.</p>
+    <p>Successfully switching to the Old Layout will change your account, so it will take effect in any browser you use, not just browsers with this extension. If you log into different accounts in this same browser, they may or may not be able to switch. The ability to switch depends on the ACCOUNT being switched.</p>
+    <p><b>Styles have been added by Old Layout</b> to the Facebook's layout you're now seeing. This is an attempt to make the new layout more familiar and usable. If you <u>can't</u> switch to the Old Layout, these fixes will at least help it be bearable.</p>
+    <p><i>To hide this popup without switching, click the "Disable this popup" button.</i><br>${faq}</p>
+    <div>
+      <button id="oldlayout-button">Switch to Old Layout</button>
+      <button id="oldlayout-disable-button">Disable This Popup</button>
+    </div>
+    `;
+  if (/oldlayout=true/.test(location.href)) {
+    message = `<p>Oops! It looks like we tried to switch your account but it didn't actually succeed.</p>
+        <p>This happens for some accounts, and we don't know why. This isn't a problem with the Old Layout extension, but a limitation of Facebook itself. It is preventing some accounts from switching.</p>
+        <p>Hopefully the styles applied to the new layout will at least be an improvement for you.</p>
+        <p>The extension might still work for other accounts on this same browser. Unfortunately we don't yet know why some accounts can't be switched, but we're working on it.</p>
+        <p>For now, you can click to hide the panel below and it will go away until a new version is released.</p>
+        <p>${faq}</p>
+        <p>${review_note}</p>
+      <button id="oldlayout-disable-button">Disable This Popup</button>
+        `;
+  }
+  let div = document.createElement("div");
+  div.id = "oldlayout";
+  div.innerHTML = DOMPurify.sanitize(`
+      <div id="oldlayout-header">Old Layout ${version} <span id="oldlayout-release-date">(${date})</span></div>
+      <div id="oldlayout-msg">${message}</div>
+  `);
   document.body.appendChild(div);
+  try {
+    if ($('#oldlayout-button')) {
+      $('#oldlayout-button').addEventListener('click', old_layout_switch);
+    }
+    $('#oldlayout-disable-button').addEventListener('click', disable_popup);
+  } catch (e) {
+    alert(e);
+  }
+
 }
 function disable_popup() {
   chrome.storage.local.set({"disabled":version});
   document.querySelector('#oldlayout').style.display="none";
+  alert("You can make the popup show again by using the extension's Options menu in your browser's toolbar");
 }
 function msg(m) {
-  document.querySelector('#oldlayout-msg').textContent = m;
+  document.querySelector('#oldlayout-msg').innerHTML = DOMPurify.sanitize(m);
 }
 async function old_layout_switch() {
   let dtsg = null;
@@ -65,12 +97,21 @@ async function old_layout_switch() {
     c_user = document.cookie.match(/c_user=([0-9]+)/)[1];
   } catch(e) { }
 
-  let common = "Unfortunately the current fix isn't working for everyone, but we're working on it.\nClick the Hide button to hide this popup.\nIf an update to Old Layout is released, this popup will appear again so you can try again.";
+  if (simulate_missing_data) {
+    c_user = null;
+  }
+
+  let common = `Unfortunately the current fix isn't working for everyone, but we're still working on it. It appears that Facebook does NOT allow some accounts to switch back, but we don't know why. This is beyond Old Layout's control. <br>
+Click the "Disable This Popup" button below to hide this popup for now.<br><br>
+If an update to Old Layout is released, this popup will appear again so you can try again.<br><br>
+${review_note}<br><br>
+`;
+
   if (!dtsg || !c_user) {
-    let s = "Old Layout could not find the necessary data to switch!\nMissing: ";
+    let s = "Old Layout could not find the necessary data to switch!<br>Missing: ";
     if (!dtsg) { s += "dtsg "; }
     if (!c_user) { s+= "userid "; }
-    s+= "\n" + common;
+    s+= `<br><br>${common}${faq}<br><button id="oldlayout-disable-button">Disable This Popup</button>`;
     msg(s);
     document.querySelector('#oldlayout-button').style.display="none";
     return;
@@ -88,16 +129,21 @@ async function old_layout_switch() {
     ];
 
     let fire = function() {
+      console.log("Trying "+index);
       const xhr = new XMLHttpRequest();
       xhr.open('POST', 'https://www.facebook.com/api/graphql/');
       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       xhr.setRequestHeader('Referrer', 'https://www.facebook.com/');
       xhr.addEventListener('readystatechange', () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
+          //console.log(index);
+          //console.log(xhr.responseText);
           if (!/"success":true/.test(xhr.responseText)) {
             console.log(xhr.responseText);
-            if (++index>urls.length) {
-              msg("Sorry, but the switch to Old Layout failed.\n"+common);
+            console.log(index);
+            console.log(urls.length);
+            if (++index>urls.length-1) {
+              msg(`Sorry, but the switch to Old Layout failed.<br><br>${common}${faq}<br><button id="oldlayout-disable-button">Disable This Popup</button>`);
               document.querySelector('#oldlayout-button').style.display="none";
             }
             else {
@@ -105,12 +151,21 @@ async function old_layout_switch() {
             }
           } else {
             //alert("ID "+index+" worked ");
-            location.reload(true);
+            location.href = "https://www.facebook.com/?oldlayout=true";
           }
         }
       });
-      xhr.send(urls[index]);
+      let url = urls[index];
+      if (simulate_failure) {
+        url = url.replace("OPT_OUT","XXXXXX");
+      }
+      xhr.send(url);
     };
-    fire();
+    if (simulate_false_positive) {
+      location.href = "https://www.facebook.com/?oldlayout=true";
+    }
+    else {
+      fire();
+    }
   }
 }
